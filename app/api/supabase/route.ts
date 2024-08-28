881f997b7974f9f33bbb71388a80f33f3e0911e2
jluo <jluo8@wgu.edu>
2024-08-27 20:32:47 -0700

    supabase

--- app/api/supabase/route.ts
+++ app/api/supabase/route.ts
@@ -1,12 +1,17 @@
 import db from "@/utils/db";
 import { NextResponse, NextRequest } from "next/server";
 
-export async function GET(request: NextRequest) {
-  const products = await db.product.findMany({
-    where: {
-      name: "Scandinavian Bed Frame",
-    },
-  });
+export async function GET(req: NextRequest, res: NextResponse) {
+  try {
+    const products = await db.product.findMany({
+      where: {
+        name: "Scandinavian Bed Frame",
+      },
+    });
 
-  return NextResponse.json(products);
+    return NextResponse.json({ message: "Supabase accessed successfully", products });
+  } catch (error) {
+    console.error("Error accessing Supabase:", error);
+    NextResponse.error();
+  }
 }
